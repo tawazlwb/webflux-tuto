5b9b6a931bcf22059114375334b9e1e5f1e0e5c9
Khairy Ismail <ikheiry@sqli.com>
2023-06-06 13:37:03 +0200

    webflux-tuto init

---
+++ src/test/java/com/ikhairy/webfluxtuto/WebfluxTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikhairy.webfluxtuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
