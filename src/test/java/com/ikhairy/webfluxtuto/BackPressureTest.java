8be3d940a2b771637bafd8cc2e14abd061dc2143
Khairy Ismail <ikheiry@sqli.com>
2023-06-07 19:05:55 +0200

    webflux-tuto testBackPressureDrop test

--- src/test/java/com/ikhairy/webfluxtuto/BackPressureTest.java
+++ src/test/java/com/ikhairy/webfluxtuto/BackPressureTest.java
@@ -17,6 +17,7 @@ public class BackPressureTest {
 
         numbers.subscribe(new BaseSubscriber<Integer>() {
             private int count = 0;
+
             @Override
             protected void hookOnSubscribe(Subscription subscription) {
                 request(3);
@@ -47,5 +48,47 @@ public class BackPressureTest {
                 super.hookOnCancel();
             }
         });
+    }
+
+    @Test
+    void testBackPressureDrop() {
+        Flux<Integer> numbers = Flux.range(1, 100).log();
+        numbers
+                .onBackpressureDrop(integer -> {
+                    System.out.println("Dropped value = " + integer);
+                })
+                .subscribe(new BaseSubscriber<Integer>() {
+                    private int count = 0;
+
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(3);
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        ++count;
+                        System.out.println("value = " + value);
+
+                        if (count == 3) {
+                            hookOnCancel();
+                        }
+                    }
+
+                    @Override
+                    protected void hookOnComplete() {
+                        System.out.println("Completed!");
+                    }
+
+                    @Override
+                    protected void hookOnError(Throwable throwable) {
+                        super.hookOnError(throwable);
+                    }
+
+                    @Override
+                    protected void hookOnCancel() {
+                        super.hookOnCancel();
+                    }
+                });
     }
 }
